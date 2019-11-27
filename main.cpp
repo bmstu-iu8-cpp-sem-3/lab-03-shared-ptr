c7dc8a5160f1949c7e1ee1bbadbe7dfc1659c795
ARAOvsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2019-11-27 22:00:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include "SharedPtr.hpp"
 
-
-int main(int argc, char *argv[]) {
-    try {
-        int number = 100;
-        // Create a shared_ptr
-        SharedPtr<int> p1;
-        SharedPtr<int> p2;
-        *p2 = number;
-        p1 = p2;
-        EXPECT_TRUE(*p1 == *p2);
-        EXPECT_TRUE(p1.use_count() == p2.use_count());
-    } catch (const std::invalid_argument& e) {
-        std::cout << "test failed! " << e.what() << std::endl;
-        return 1;
-    }
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-//    return 0;
+int main() 
+{
+   return 0;
 }
