790ea4416295c95502e8c9d470e12135a66cb7bf
progrm_jarvis <gh@progrm-jarvis.ru>
2020-10-10 22:34:43 +0300

    fix: use `gmock_main` target instead of `gtest`

---
+++ tests/shared_pointer_test.cpp
@@ -0,0 +1,11 @@
+// Copyright 2020 Petr Portnov <gh@progrm-jarvis.ru>
+
+#include <gtest/gtest.h>
+
+namespace shared_pointer {
+
+    TEST(SharedPointer, Example) {
+        ASSERT_TRUE(true);
+    }
+}
+
