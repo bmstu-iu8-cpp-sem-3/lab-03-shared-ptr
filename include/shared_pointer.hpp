bd13ed3d8df79c23555e3846cf57671ec29a7b37
progrm_jarvis <gh@progrm-jarvis.ru>
2020-10-11 01:03:00 +0300

    feat: implement shared pointer

--- include/shared_pointer.hpp
+++ include/shared_pointer.hpp
@@ -5,8 +5,11 @@
 
 #include <atomic>
 #include <memory>
+#include <utility>
 
 namespace shared_pointer {
+
+    /* **************************************************** API **************************************************** */
 
     template <typename T>
     class SharedPointer final {
@@ -54,6 +57,96 @@ namespace shared_pointer {
 
     template <typename T, typename... ConstructorArguments>
     inline SharedPointer<T> make_shared(ConstructorArguments&&... constructorArguments);
+
+    /* ********************************************** Implementations ********************************************** */
+
+    template <typename T>
+    void SharedPointer<T>::ControlBlock::acquireStrong_() {
+        ++strongRefCount_;
+    }
+
+    template <typename T>
+    void SharedPointer<T>::ControlBlock::releaseStrong_() {
+        if (!--strongRefCount_) {
+            delete rawPointer_;
+            delete this; // this is legal (!!!)
+        }
+    }
+
+    template <typename T>
+    SharedPointer<T>::ControlBlock::ControlBlock(T* const rawPointer) : rawPointer_(rawPointer), strongRefCount_(1) {}
+
+    template <typename T>
+    SharedPointer<T>::SharedPointer() : rawPointer_(nullptr), controlBlock_(nullptr) {}
+
+    template <typename T>
+    SharedPointer<T>::SharedPointer(T*&& rawPointer)
+        : rawPointer_(rawPointer), controlBlock_(new ControlBlock(rawPointer)) {}
+
+    template <typename T>
+    template <typename... ConstructorArguments>
+    SharedPointer<T>::SharedPointer(ConstructorArguments&&... constructorArguments)
+        : SharedPointer(new T{constructorArguments...}) {}
+
+    // Add a new strong owner
+    template <typename T>
+    SharedPointer<T>::SharedPointer(const SharedPointer& original)
+        : rawPointer_(original.rawPointer_), controlBlock_(original.controlBlock_) {
+        if (controlBlock_) controlBlock_->acquireStrong_();
+    }
+
+    // Exchange ownership
+    template <typename T>
+    SharedPointer<T>::SharedPointer(SharedPointer&& original) noexcept
+        : rawPointer_(std::exchange(original.rawPointer_, nullptr)),
+          controlBlock_(std::exchange(original.controlBlock_, nullptr)) {}
+
+    template <typename T>
+    SharedPointer<T>::~SharedPointer() {
+        if (auto const controlBlock = controlBlock_) controlBlock->releaseStrong_();
+    }
+
+    template <typename T>
+    SharedPointer<T>& SharedPointer<T>::operator=(const SharedPointer& other) noexcept {
+        if (&other != this) {
+            rawPointer_ = other.rawPointer_;
+            auto const newControlBlock = other.controlBlock_;
+            if (auto oldControlBlock = std::exchange(controlBlock_, newControlBlock)) oldControlBlock->releaseStrong_();
+            newControlBlock->acquireStrong_();
+        }
+
+        return *this;
+    }
+
+    template <typename T>
+    SharedPointer<T>& SharedPointer<T>::operator=(SharedPointer&& other) noexcept {
+        if (&other != this) {
+            std::swap(rawPointer_, other.rawPointer_);
+            std::swap(controlBlock_, other.controlBlock_);
+        }
+
+        return *this;
+    }
+    template <typename T>
+    SharedPointer<T>::operator bool() const noexcept {
+        return rawPointer_;
+    }
+
+    template <typename T>
+    T& SharedPointer<T>::operator*() const noexcept {
+        return *rawPointer_;
+    }
+
+    template <typename T>
+    T* SharedPointer<T>::operator->() const noexcept {
+        return rawPointer_;
+    }
+
+    template <typename T, typename... ConstructorArguments>
+    SharedPointer<T> make_shared(ConstructorArguments&&... constructorArguments) {
+        return SharedPointer<T>(std::forward<ConstructorArguments>(constructorArguments)...);
+    }
+
 } // namespace shared_pointer
 
 #endif // INCLUDE_SHARED_POINTER_HPP_
--- tests/shared_pointer_test.cpp
+++ tests/shared_pointer_test.cpp
@@ -2,10 +2,124 @@
 
 #include <gtest/gtest.h>
 
+#include <shared_pointer.hpp>
+#include <string>
+
 namespace shared_pointer {
 
-    TEST(SharedPointer, Example) {
-        ASSERT_TRUE(true);
+    inline static void assertNotDestroyed(bool const destroyed) {
+        if (destroyed) FAIL() << "Destructor has already been called";
     }
-}
 
+    inline static void assertDestroyed(bool const destroyed) {
+        if (!destroyed) FAIL() << "Destructor has not been called";
+    }
+
+    struct DestructionMonitor {
+        /*[[maybe_unused]]*/ bool &destroyed;
+        int value;
+
+        ~DestructionMonitor() { destroyed = true; }
+    };
+
+    TEST(SharedPointer, Dereference) {
+        SharedPointer<std::string> const sharedPointer("Hello world");
+        ASSERT_EQ(*sharedPointer, "Hello world");
+    }
+
+    TEST(SharedPointer, OperatorBool) {
+        ASSERT_FALSE(SharedPointer<int>());
+        ASSERT_TRUE(SharedPointer<int>(123));
+    }
+
+    TEST(SharedPointer, ConstructorFromVaarg) {
+        bool monitor = false;
+        {
+            SharedPointer<DestructionMonitor> const sharedPointer(monitor, 6274);
+            ASSERT_EQ(sharedPointer->value, 6274);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+
+    TEST(SharedPointer, ConstructorFromRaw) {
+        bool monitor = false;
+        {
+            SharedPointer<DestructionMonitor> const sharedPointer(new DestructionMonitor{monitor, 1234});
+            ASSERT_EQ(sharedPointer->value, 1234);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+
+    TEST(SharedPointer, MakeShared) {
+        bool monitor = false;
+        {
+            auto const sharedPointer = make_shared<DestructionMonitor>(monitor, 3264);
+            ASSERT_EQ(sharedPointer->value, 3264);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+
+    TEST(SharedPointer, CopyConstructor) {
+        bool monitor = false;
+        {
+            SharedPointer<DestructionMonitor> const sharedPointer(new DestructionMonitor{monitor, 1467});
+            ASSERT_EQ(sharedPointer->value, 1467);
+            {
+                SharedPointer<DestructionMonitor> const otherSharedPointer(sharedPointer);
+                ASSERT_EQ(sharedPointer->value, 1467);
+                ASSERT_EQ(otherSharedPointer->value, 1467);
+                assertNotDestroyed(monitor);
+            }
+            ASSERT_EQ(sharedPointer->value, 1467);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+
+    TEST(SharedPointer, CopyAssignment) {
+        bool monitor = false;
+        {
+            SharedPointer<DestructionMonitor> const sharedPointer(new DestructionMonitor{monitor, 4728});
+            ASSERT_EQ(sharedPointer->value, 4728);
+            {
+                SharedPointer<DestructionMonitor> otherSharedPointer;
+                otherSharedPointer = sharedPointer;
+                ASSERT_EQ(sharedPointer->value, 4728);
+                ASSERT_EQ(otherSharedPointer->value, 4728);
+                assertNotDestroyed(monitor);
+            }
+            ASSERT_EQ(sharedPointer->value, 4728);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+
+    TEST(SharedPointer, MoveConstructor) {
+        bool monitor = false;
+        {
+            SharedPointer<DestructionMonitor> sharedPointer(
+                SharedPointer<DestructionMonitor>(new DestructionMonitor{monitor, 6847}));
+            ASSERT_EQ(sharedPointer->value, 6847);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+
+    TEST(SharedPointer, MoveAssignment) {
+        bool monitor = false;
+        {
+            SharedPointer<DestructionMonitor> sharedPointer;
+            {
+                SharedPointer<DestructionMonitor> otherSharedPointer = SharedPointer<DestructionMonitor>(new DestructionMonitor{monitor, 7412});
+                sharedPointer = std::move(otherSharedPointer);
+                ASSERT_FALSE(otherSharedPointer);
+            }
+            ASSERT_EQ(sharedPointer->value, 7412);
+            assertNotDestroyed(monitor);
+        }
+        assertDestroyed(monitor);
+    }
+} // namespace shared_pointer
